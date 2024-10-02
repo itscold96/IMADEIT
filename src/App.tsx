820d16e4fb7a894135ad5cd83cd882b75a892223
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-02 16:29:09 +0900

    #1 env: CRA 설치 및 필요 라이브러리, 전역 폰트 설정
    - 설치 라이브러이
        "@tanstack/react-query": "^5.56.2",
        "classnames": "^2.5.1",
        "react": "^18.3.1",
        "typescript": "^4.9.5",
        "zustand": "^5.0.0-rc.2"
        "tailwindcss": "^3.4.13"
    
    - 전역 폰트: Pretandard

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className={'text-5xl'}>
+      <div className={'font-thin'}>HOME, my sweet home</div>
+      <div className={'font-light'}>HOME, my sweet home</div>
+      <div className={'font-medium'}>HOME, my sweet home</div>
+      <div className={'font-semibold'}>HOME, my sweet home</div>
+      <div className={'font-bold'}>HOME, my sweet home</div>
+      <div className={'font-black'}>HOME, my sweet home</div>
+    </div>
+  );
+}
+
+export default App;
