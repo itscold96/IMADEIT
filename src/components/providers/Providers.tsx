ff8a9ba3584ed70543f522d8d0e5c07266ad604c
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-03 10:25:56 +0900

    #2 feat: tanstack-query Provider 세팅

---
+++ src/components/providers/Providers.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react';
+import QueryProvider from './QueryProvider';
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return <QueryProvider>{children}</QueryProvider>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
 import Router from 'Router';
+import Providers from 'components/providers/Providers';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Router />
+    <Providers>
+      <Router />
+    </Providers>
   </React.StrictMode>,
 );
