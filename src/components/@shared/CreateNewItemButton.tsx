6f4cab221f2688a35bf8176bdb98658a2085a166
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-03 20:03:47 +0900

    #2 design: 새로 제작한 유틸리티 클래스로 CreateNewItemButton 클래스명 변경

--- src/components/@shared/CreateNewItemButton.tsx
+++ src/components/@shared/CreateNewItemButton.tsx
@@ -7,13 +7,11 @@ interface CreateNewItemButtonProps {
 
 export default function CreateNewItemButton({ text, onCreateClick }: CreateNewItemButtonProps) {
   return (
-    <button onClick={onCreateClick} className={'flex gap-3 items-center w-full'}>
-      <div className={'flex justify-center items-center h-16 min-w-16 bg-zinc-800 rounded-md '}>
+    <button onClick={onCreateClick} className={'item-container'}>
+      <div className={'emoji-wrapper bg-zinc-800'}>
         <Plus className={'text-red-500'} size={36} strokeWidth={1.8} />
       </div>
-      <div className={'flex items-center text-red-500 text-lg border-b-[0.1px] h-16 w-full border-zinc-600'}>
-        {text}
-      </div>
+      <div className={'item-text-wrapper text-red-500 '}>{text}</div>
     </button>
   );
 }
