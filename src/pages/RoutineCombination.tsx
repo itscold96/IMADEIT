03566befd0be6a12ce057103ca04143c8cdc556d
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-05 23:11:30 +0900

    #3 design: 루틴 시작, 편집 버튼 구현

--- src/pages/RoutineCombination.tsx
+++ src/pages/RoutineCombination.tsx
@@ -1,5 +1,6 @@
 import RootLayout from 'components/@shared/RootLayout';
 import CombinationList from 'components/combination/CombinationList';
+import { Cog, Play } from 'lucide-react';
 import { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -8,7 +9,16 @@ export default function RoutineCombination() {
 
   return (
     <RootLayout>
-      RoutineCombination id: {id}
+      <div className={'flex gap-3 text-red-500 text-lg h-14'}>
+        <button className={'paly-edit-button'}>
+          <Play size={25} strokeWidth={2.2} />
+          시작
+        </button>
+        <button className={'paly-edit-button'}>
+          <Cog size={25} strokeWidth={2.2} />
+          편집
+        </button>
+      </div>
       <Suspense fallback={<>loading...</>}>
         <CombinationList linkedRoutineId={id as string} />
       </Suspense>
