5d31a8c73ef62cea1a1d81eefe39388bdb301e2c
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-09 22:11:47 +0900

    #3 feat: react-modal을 활용한 운동 생성 모달 제작

--- src/components/combination/CreateNewWorkout.tsx
+++ src/components/combination/CreateNewWorkout.tsx
@@ -1,12 +1,39 @@
 import CreateNewItemButton from 'components/@shared/CreateNewItemButton';
 import { useToggle } from 'hooks/useToggle';
+import ReactModal from 'react-modal';
+import Modal from 'react-modal';
+
+const customStyles: ReactModal.Styles = {
+  overlay: {
+    backgroundColor: ' rgba(0, 0, 0, 0.3)',
+  },
+  content: {
+    backgroundColor: 'black',
+    border: '1px solid #52525b',
+    maxWidth: '768px',
+    minWidth: '320px',
+    padding: '20px',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '15px',
+  },
+};
 
 export default function CreateNewWorkout() {
-  const { toggleValue, toggleDispatch } = useToggle();
+  const { toggleValue: isOpen, toggleDispatch } = useToggle();
 
   return (
     <>
       <CreateNewItemButton text={'새로운 운동 추가하기..'} onCreateClick={() => toggleDispatch({ type: 'switch' })} />
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => toggleDispatch({ type: 'off' })}
+        contentLabel="신규 운동을 생성하는 모달" // 스크린 리더 사용자를 위한 라벨
+        style={customStyles}
+      >
+        <div>모달!</div>
+      </Modal>
     </>
   );
 }
