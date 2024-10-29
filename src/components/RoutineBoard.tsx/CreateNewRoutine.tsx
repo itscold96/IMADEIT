58c77d66f6a482b1e37fd7c3eef7e92a5dd97d0e
Minchan <98478661+itscold96@users.noreply.github.com>
2024-10-29 17:43:23 +0900

    #3 remove: git이 대소문자 구분을 하지 않아 생긴 폴더 제거

--- src/components/RoutineBoard.tsx/CreateNewRoutine.tsx
+++
@@ -1,9 +0,0 @@
-import CreateNewItemButton from 'components/@shared/CreateNewItemButton';
-
-export default function CreateNewRoutine() {
-  const handleCreateClick = () => {
-    console.log('새로운 루틴 생성 버튼 클릭');
-  };
-
-  return <CreateNewItemButton text={'새로운 루틴 추가하기..'} onCreateClick={handleCreateClick} />;
-}
